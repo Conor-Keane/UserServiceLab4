878de4ab7da534d705cf9991296db230db0a8237
Conor-Keane <g00391846@atu.ie>
2023-10-11 10:21:49 +0100

    Added PostMapping and hash map to UserController

--- src/main/java/ie/atu/userservicelab4/userservicelab4/UserController.java
+++ src/main/java/ie/atu/userservicelab4/userservicelab4/UserController.java
@@ -1,4 +1,27 @@
 package ie.atu.userservicelab4.userservicelab4;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class UserController {
+    private RegistrationService registrationService;
+
+    @Autowired
+    public void userController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+
+    }
+
+    @PostMapping("/confirm-and-register")
+    public Map<String, String> confirmAndRegister(@RequestBody UserDetails userDetails) {
+        String confirm = registrationService.someDetails(userDetails);
+        Map<String, String> responseMessage = new HashMap<>();
+        responseMessage.put("message",confirm);
+        return responseMessage;
+    }
 }
