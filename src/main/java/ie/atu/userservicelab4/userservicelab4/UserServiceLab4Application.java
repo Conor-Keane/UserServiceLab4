2321bba7147c5a06972844ed034be2879b0677ad
Conor-Keane <g00391846@atu.ie>
2023-10-11 09:48:18 +0100

    Created Lab4 Project

---
+++ src/main/java/ie/atu/userservicelab4/userservicelab4/UserServiceLab4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.userservicelab4.userservicelab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceLab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceLab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/userservicelab4/userservicelab4/UserServiceLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.userservicelab4.userservicelab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceLab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
